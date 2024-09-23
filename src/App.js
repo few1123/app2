00e4cf98a4f5b5db33a19910b2105678fff8afb6
few1123 <angkhawee5@gmail.com>
2024-09-23 11:29:51 +0700

    งาน Router
    งาน Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import Button from './class-components'
+import Router3 from './router3'
 
-function App(){
-  return <Button/>
+export default function App(){
+  return<Router3/>
 }
-export default App
